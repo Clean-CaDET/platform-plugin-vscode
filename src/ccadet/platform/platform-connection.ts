035efb44b45287af5e52e4d85ceb256e20617eea
Nikola Luburic <luburic91@gmail.com>
2021-03-16 12:44:55 +0100

    feat: Sends code from all CS files under a folder to the platform.

--- src/ccadet/platform/platform-connection.ts
+++ src/ccadet/platform/platform-connection.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import * as path from 'path';
 import * as https from 'https';
 import axios from 'axios';
 import { ClassQualityAnalysisDTO } from './dtos/class-quality-analysis-dto';
@@ -29,15 +30,40 @@ export class PlatformConnection {
     }
 
     private loadCode(filePath: string) {
+        if(filePath.endsWith(".cs")) {
+            return this.readCodeFromFile(filePath);
+        }
+
+        const getAllFiles = function (dirPath: string, arrayOfFiles: string[]) {
+            let files = fs.readdirSync(dirPath);
+
+            files.forEach(function (file) {
+                let filePath = path.resolve(dirPath, file);
+                if (fs.statSync(filePath).isDirectory()) {
+                    arrayOfFiles = getAllFiles(filePath, arrayOfFiles);
+                } else if(filePath.endsWith(".cs")) arrayOfFiles.push(filePath);
+            });
+
+            return arrayOfFiles;
+        }
+
+        let files = getAllFiles(filePath, []);
+        return Promise.all(files.map(this.readCodeFromFile))
+    }
+
+    private readCodeFromFile(filePath: string) {
         return new Promise((resolve, reject) => {
             fs.promises.readFile(filePath)
-                .then(resolve)
+                .then(sourceCode => {
+                    resolve(sourceCode.toString());
+                })
                 .catch(reject);
         });
     }
 
     private sendCode(sourceCode: any) {
-        let sc = JSON.stringify(sourceCode.toString());
+        if(typeof sourceCode == 'string') sourceCode = [sourceCode];
+        let sc = JSON.stringify(sourceCode);
 
         return new Promise((resolve, reject) => {
             http.post("repository/education/class", sc, {headers: {'Content-Type': 'application/json; charset=utf-8'}})
